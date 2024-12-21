2da624425cb3d1895041ae80874293f54bb86c2a
Nina <nina@andruppedersen.dk>
2024-12-21 10:58:17 +0100

    Add equation checker

--- src/AoC/Day07.hs
+++ src/AoC/Day07.hs
@@ -3,15 +3,34 @@ module AoC.Day07 (solution) where
 import Data.List.Split (splitOn)
 import Util.Parsing (splitStringAt)
 
-data Operations = Add | Mult
+data Operation = Add | Mult deriving (Show)
 
 type Equation = (Int, [Int])
 
+compute :: Int -> Int -> Operation -> Int
+compute x y Add = x + y
+compute x y Mult = x * y
+
+operationCombinations :: Int -> [[Operation]]
+operationCombinations 0 = [[]]
+operationCombinations n =
+  [x : xs | x <- [Add, Mult], xs <- operationCombinations (n - 1)]
+
+equationHolds :: Equation -> [Operation] -> Bool
+equationHolds (res, [x, y]) [oper] = res == compute x y oper
+equationHolds (res, x : y : valueTail) (oper : operTail) =
+  equationHolds (res, compute x y oper : valueTail) operTail
+equationHolds _ _ = False
+
+equationPossible :: Equation -> Bool
+equationPossible equation =
+  any (equationHolds equation) (operationCombinations (length (snd equation) - 1))
+
 parseEquation :: String -> Equation
 parseEquation str =
-  let (res, values) = splitStringAt ':' str
+  let (testValue, values) = splitStringAt ':' str
       values' = splitOn " " values
-   in (read res, map read (tail values'))
+   in (read testValue, map read (tail values'))
 
 solution :: [String] -> [Equation]
 solution = map parseEquation
