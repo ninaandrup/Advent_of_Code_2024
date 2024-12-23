7e8de480d36956136c47ec090a92628331736d85
Nina <nina@andruppedersen.dk>
2024-12-23 09:13:02 +0100

    Add template for day 09

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import AoC.Day05 (solution)
 import AoC.Day06 (solution)
 import AoC.Day07 (solution1, solution2)
+import AoC.Day09 (solution1, solution2)
 import System.Environment (getArgs)
 
 main :: IO ()
@@ -15,17 +16,13 @@ main = do
           contents <- readFile ("input/day" ++ day ++ tpe' ++ ".txt")
           let contentLines = lines contents
 
+          let toPrint sol1 sol2 = "Part I: " ++ show sol1 ++ "\nPart II: " ++ show sol2
+
           case day of
             "05" -> print (AoC.Day05.solution contentLines)
             "06" -> print (AoC.Day06.solution contentLines)
-            "07" ->
-              putStrLn
-                ( "Part I: "
-                    ++ ( show (AoC.Day07.solution1 contentLines)
-                           ++ "\nPart II: "
-                           ++ show (AoC.Day07.solution2 contentLines)
-                       )
-                )
+            "07" -> putStrLn (toPrint (AoC.Day07.solution1 contentLines) (AoC.Day07.solution2 contentLines))
+            "09" -> putStrLn (toPrint (AoC.Day09.solution1 contentLines) (AoC.Day09.solution2 contentLines))
             _ -> putStrLn "Day is not implemented"
       )
     _ -> putStrLn "Wrong number of arguments"
---
+++ src/AoC/Day09.hs
@@ -0,0 +1,7 @@
+module AoC.Day09 (solution1, solution2) where
+
+solution1 :: [String] -> Int
+solution1 input = 0
+
+solution2 :: [String] -> Int
+solution2 input = 0
